c49c3aa674f37948d43846f7301e98772f0bef2e
IrfanEzani <irfanezani@gmail.com>
2021-06-15 20:48:57 +0800

    complete utopian tree code

--- Java/ProblemSolving/utopianTree.java
+++ Java/ProblemSolving/utopianTree.java
@@ -3,19 +3,32 @@ import java.util.*;
 
 class Test8 {
     public static int utopianTree(int n) {
-    
-    return 0;
+    int height = 1;
+    for(int i = height; i <= n; i++) {
+        if (i % 2 == 0) {
+            height++;
+            /* System.out.println("new height val:" + height); */
+        } else if (i%2 != 0) {
+            height*=2;
+           /*  System.out.println("new height val:" + height); */
+        }
+    }
+    if (n == 0) {
+        height += 0;
+        /* System.out.println("new height val:" + height); */
+    }
+    return height;
     }
 }
 public class utopianTree {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        System.out.println("total case:" + n);
+        /* System.out.println("total case:" + n); */
         for(int i = 0; i < n; i++) {
             int temp = s.nextInt();
-            System.out.println("next int val:" + temp);
-            Test8.utopianTree(temp);
+            /* System.out.println("temp value:" + temp); */
+            System.out.println(Test8.utopianTree(temp)); 
         }
         s.close();
     }
