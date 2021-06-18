f13fa66290fb7162c7ec003fd24383f9f5b86f8d
IrfanEzani <irfanezani@gmail.com>
2021-06-19 05:36:30 +0800

    subarray

---
+++ Java/subArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class subArray {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int len = s.nextInt();
+        int counter = 0;
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    System.out.print("(" + arr[k] + ")");
+                    sum += arr[k];
+                }
+                if(sum < 0) {
+                    counter++;
+                }
+                System.out.println("Sum: " + sum);
+                System.out.println("counter:" + counter);
+            }
+        }
+        System.out.println("\nFinal counter=" + counter);
+    }
+}
